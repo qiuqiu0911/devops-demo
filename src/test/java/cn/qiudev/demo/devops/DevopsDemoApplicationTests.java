d5349ca788ff50d4a4d7dee902ad12b238e49b79
qiuqiu <qiu_zs@126.com>
2023-04-15 08:46:54 +0800

    init

---
+++ src/test/java/cn/qiudev/demo/devops/DevopsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.qiudev.demo.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
